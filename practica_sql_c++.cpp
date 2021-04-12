399ea2b68a055dcf7aaf185beadf99594208a8fa
Keren Rodríguez <krodriguezg4@miumg.edu.gt>
2021-04-11 22:48:37 -0600

    c++ y mysql

---
+++ practica_sql_c++.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include<mysql.h>
+
+using namespace std;
+
+int q_estado;
+
+int main()
+{
+    int opc;
+    MYSQL* conectar;//variable tipo mysql
+    MYSQL_ROW fila;
+    MYSQL_RES* resultado;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "jyzene", "Ocarinaoft!me123", "db_producto", 3306, NULL, 0);
+    if (conectar) {
+        cout << "Selecciona la opcion que desees realizar" << endl;
+        cout << "1) Insertar datos" << endl;
+        cout << "2) Leer datos " << endl;
+        cout << "0) salir" << endl;
+        cin >> opc;
+
+        switch (opc) {
+            case 1:
+                string producto;
+                cout << "Ingrese el producto: ";
+                cin >> producto;
+                string insert = "Insert into producto(producto) values('" + producto + "')";
+                const char* i = insert.c_str();
+                q_estado = mysql_query(conectar, i);
+                if (!q_estado) {
+                    cout << "si conecto!" << endl;
+                }
+                else {
+                    cout << "hubo un error :(" << endl;
+                }
+                break;
+            case 2:
+                string consulta = "select * from producto";
+                const char* c = consulta.c_str();
+                q_estado = mysql_query(conectar, c);
+                if (!q_estado) {
+                    resultado = mysql_store_result(conectar);
+                    while (fila = mysql_fetch_row(resultado)) {
+                        cout << fila[0] << "," << fila[1] << endl;
+                    }
+                }
+                break;
+            case 0:
+                break;
+        
+        }
+    }
+    else {
+        cout << "rayos, algo fallo :(" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
---
+++ script/db_productos_producto.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_productos
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `producto` (
+  `idproducto` int NOT NULL AUTO_INCREMENT,
+  `producto` varchar(50) DEFAULT NULL,
+  `idmarca` smallint DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `precio_costo` decimal(8,2) DEFAULT NULL,
+  `precio_venta` decimal(8,2) DEFAULT NULL,
+  `existencia` int DEFAULT NULL,
+  `fecha_ingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idproducto`),
+  KEY `idmarca_marca_producto_idx` (`idmarca`),
+  CONSTRAINT `idmarca_marca_producto` FOREIGN KEY (`idmarca`) REFERENCES `marca` (`idmarca`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+INSERT INTO `producto` VALUES (1,'Tennis',1,'blancos para caballero deportivos',180.00,280.00,30,NULL),(2,'Computadora',2,'procesador i5 intel, 8gb de ram',3180.00,4280.00,3,NULL),(3,'Gorra',1,'Gorra color negro para dama',190.00,300.00,12,NULL),(4,'iPhone',3,'iPhone X',8000.00,10000.00,4,NULL),(5,'iPhone',3,'iPhone 6S Rosegold',3000.00,4000.00,3,NULL),(6,'iPhone',3,'iPhone 7 Rojo',4000.00,5000.00,8,NULL);
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-11 22:38:18
